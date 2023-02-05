8f94c2e6738fa6e255f61caaedddf22aa52c2abb
Igor Liferenko <igor.liferenko@gmail.com>
2023-02-05 16:54:01 +0700

    edit

--- newline.c
+++ newline.c
@@ -10,7 +10,12 @@ int main(void)
   tcgetattr(STDIN_FILENO, &termios);
   termios.c_lflag &= ~(ECHO | ICANON);
   tcsetattr(STDOUT_FILENO, TCSANOW, &termios);
-
+  write(STDOUT_FILENO, "\e[6n", 4);
+  char row[4], col[4];
+  row[0] = row[1] = row[2] = row[3] = 0;
+  col[0] = col[1] = col[2] = col[3] = 0;
+  read(STDIN_FILENO, ...
+  if (col[1] != 0 || col[0] != '1') write(STDOUT_FILENO, "\n", 1);
   termios.c_lflag |= ECHO | ICANON;
   tcsetattr(STDOUT_FILENO, TCSANOW, &termios);
   return 0;
