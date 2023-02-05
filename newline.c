3464af66a8a8b31abefc236ac68c1372001b12e0
Igor Liferenko <igor.liferenko@gmail.com>
2023-02-05 18:06:00 +0700

    implement newline in C

--- newline.c
+++ newline.c
@@ -11,12 +11,29 @@ int main(void)
   termios.c_lflag &= ~(ECHO | ICANON);
   tcsetattr(STDOUT_FILENO, TCSANOW, &termios);
   write(STDOUT_FILENO, "\e[6n", 4);
-  char row[4], col[4];
+  char c, row[4], *rp, col[4], *cp;
+reset1:
+  read(STDIN_FILENO, &c, 1);
+reset2:
+  if (c != '\e') goto reset1;
+  read(STDIN_FILENO, &c, 1);
+  if (c != '[') goto reset2;
   row[0] = row[1] = row[2] = row[3] = 0;
-  col[0] = col[1] = col[2] = col[3] = 0;
-  read(STDIN_FILENO, ...
-  if (col[1] != 0 || col[0] != '1') write(STDOUT_FILENO, "\n", 1);
+  rp = row;
+row:
+  read(STDIN_FILENO, &c, 1);
+  if (c >= '0' && c <= '9') { *rp++ = c; goto row; }
+  if (c != ';') goto reset2;
+  if (rp == row) goto reset1;
+  col[0] = col[1] = col[2] = col[3] = 0; 
+  cp = col;
+col:
+  read(STDIN_FILENO, &c, 1);
+  if (c >= '0' && c <= '9') { *cp++ = c; goto col; }
+  if (c != 'R') goto reset2;
+  if (cp == col) goto reset1;
   termios.c_lflag |= ECHO | ICANON;
   tcsetattr(STDOUT_FILENO, TCSANOW, &termios);
+  if (col[1] != 0 || col[0] != '1') write(STDOUT_FILENO, "\n", 1);
   return 0;
 }
