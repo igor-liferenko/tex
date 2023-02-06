9015835862899fd6f16efb8515778bb822ccffc8
Igor Liferenko <igor.liferenko@gmail.com>
2023-02-06 19:56:27 +0700

    edit

--- newline.c
+++ newline.c
@@ -11,29 +11,27 @@ int main(void)
   termios.c_lflag &= ~(ECHO | ICANON);
   tcsetattr(STDOUT_FILENO, TCSANOW, &termios);
   write(STDOUT_FILENO, "\e[6n", 4);
-  char c, row[4], *rp, col[4], *cp;
+  char b, row[1], *rowp, col[1], *colp;
 reset1:
-  read(STDIN_FILENO, &c, 1);
+  read(STDIN_FILENO, &b, 1);
 reset2:
-  if (c != '\e') goto reset1;
-  read(STDIN_FILENO, &c, 1);
-  if (c != '[') goto reset2;
-  row[0] = row[1] = row[2] = row[3] = 0;
-  rp = row;
+  if (b != '\e') goto reset1;
+  read(STDIN_FILENO, &b, 1);
+  if (b != '[') goto reset2;
+  rowp = row;
 row:
-  read(STDIN_FILENO, &c, 1);
-  if (c >= '0' && c <= '9') { *rp++ = c; goto row; }
-  if (c != ';') goto reset2;
-  if (rp == row) goto reset1;
-  col[0] = col[1] = col[2] = col[3] = 0; 
-  cp = col;
+  read(STDIN_FILENO, &b, 1);
+  if (b >= '0' && b <= '9') { if (rowp == row) *row = b; rowp++; goto row; }
+  if (b != ';') goto reset2;
+  if (rowp == row) goto reset1;
+  colp = col;
 col:
-  read(STDIN_FILENO, &c, 1);
-  if (c >= '0' && c <= '9') { *cp++ = c; goto col; }
-  if (c != 'R') goto reset2;
-  if (cp == col) goto reset1;
+  read(STDIN_FILENO, &b, 1);
+  if (b >= '0' && b <= '9') { if (colp == col) *col = b; colp++; goto col; }
+  if (b != 'R') goto reset2;
+  if (colp == col) goto reset1;
   termios.c_lflag |= ECHO | ICANON;
   tcsetattr(STDOUT_FILENO, TCSANOW, &termios);
-  if (col[1] != 0 || col[0] != '1') write(STDOUT_FILENO, "\n", 1);
+  if (colp != col + 1 || *col != '1') write(STDOUT_FILENO, "\n", 1);
   return 0;
 }
